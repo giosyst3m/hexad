ba59167a5246cd62307d6769be2e85b89416babf
Giosyst3m <me@giosyst3m.net>
2019-03-30 10:13:22 -0500

    Config App Reducers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { HttpClientModule } from '@angular/common/http';
 // NgRx
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-
+import { appReducers } from './store/app.reducer';
 
 
 
@@ -31,6 +31,11 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
     PagesModule,
     ServiceModule,
     HttpClientModule,
+    StoreModule.forRoot(appReducers),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
   ],
   providers: [Title],
   bootstrap: [AppComponent]
