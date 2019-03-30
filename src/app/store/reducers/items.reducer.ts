16de0308421356879bd50a650e30391ce2cb9638
Giosyst3m <me@giosyst3m.net>
2019-03-30 10:04:16 -0500

    Items Reducer

---
+++ src/app/store/reducers/items.reducer.ts
@@ -0,0 +1,44 @@
+import { Item } from "src/app/models/item.model";
+import * as fromItems from '../actions'
+
+
+export interface ItemsState {
+    items: Item[];
+    loaded: boolean;
+    loading: boolean;
+    error: any;
+}
+
+const stateInit: ItemsState = {
+    items: [],
+    loaded: false,
+    loading: false,
+    error: null
+}
+
+export function itemsReducer( state = stateInit, action: fromItems.itemsActions ): ItemsState {
+
+    switch (action.type) {
+        case fromItems.LOAD_ITEMS:
+            return {
+                ...state,
+                loading: true
+            };
+        case fromItems.LOAD_ITEMS_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                loaded: true,
+                items: [...action.items]
+            };
+        case fromItems.LOAD_ITEMS_ERROR:
+            return {
+                ...state,
+                loaded: false,
+                loading: false,
+                error: action.payload
+            };
+        default:
+            return state;
+    }
+}
